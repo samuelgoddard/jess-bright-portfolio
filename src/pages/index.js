876c723cf9871a48169f23508d92a9e374a0babb
Sam Goddard <hello@samgoddard.co.uk>
2020-10-30 12:47:57 +0000

    testing incrememental builds

--- src/pages/index.js
+++ src/pages/index.js
@@ -56,7 +56,7 @@ const IndexPage = () => {
 
           <h2>Lorem ipsum dolor sit amet</h2>
           
-          <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anismat id est laborum.</p>
+          <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat.</p>
         </motion.div>
       </motion.section>
     </>
